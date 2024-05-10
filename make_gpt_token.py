3268787ce676c820749b60f3c62746f691269d4b
NickyBricky <buninkola16@gmail.com>
2024-05-10 17:05:50 +0300

    make some view-code fixes

--- make_gpt_token.py
+++ make_gpt_token.py
@@ -7,7 +7,6 @@ import os
 # подтягиваем константы из config-файла
 from config import LOGS
 
-TOKEN_PATH = "/home/student/Almighty_gpt_bot/creds/token.json"
 # настраиваем запись логов в файл
 logging.basicConfig(filename=LOGS, level=logging.INFO,
                     format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
@@ -22,24 +21,35 @@ def get_token():
     return iam_token
 
 
+TOKEN_PATH = "/home/student/Almighty_gpt_bot/creds/token.json"
+
+
 # получение нового iam_token
 def create_new_token():
     url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
+
     headers = {
         "Metadata-Flavor": "Google"
     }
+
     try:
         response = requests.get(url=url, headers=headers)
+
         if response.status_code == 200:
             token_data = response.json()  # вытаскиваем из ответа iam_token
+
             # добавляем время истечения iam_token к текущему времени
             token_data['expires_at'] = time.time() + token_data['expires_in']
+
             # записываем iam_token в файл
             with open(TOKEN_PATH, "w") as token_file:
                 json.dump(token_data, token_file)
+
             logging.info("Получен новый iam_token")
+
         else:
             logging.error(f"Ошибка получения iam_token. Статус-код: {response.status_code}")
+
     except Exception as e:
         logging.error(f"Ошибка получения iam_token: {e}")
 
@@ -51,11 +61,14 @@ def get_creds():
         with open(TOKEN_PATH, 'r') as f:
             file_data = json.load(f)
             expiration = datetime.strptime(file_data["expires_at"][:26], "%Y-%m-%dT%H:%M:%S.%f")
+
         # если срок годности истёк
         if expiration < datetime.now():
             logging.info("Срок годности iam_token истёк")
+
             # получаем новый iam_token
             create_new_token()
+
     except:
         # если что-то пошло не так - получаем новый iam_token
         create_new_token()
@@ -64,4 +77,5 @@ def get_creds():
     with open(TOKEN_PATH, 'r') as f:
         file_data = json.load(f)
         iam_token = file_data["access_token"]
+
     return iam_token
